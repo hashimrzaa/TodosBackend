55b6a2e16e55f7bb238d1e8c3b7100eaefff766d
Hashimrzaa <140230480+hashimrzaa@users.noreply.github.com>
2024-02-23 02:38:44 +0500

    update

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 3000;
 
-app.get('/',(req,res)=>{
-res.send('Home')
-})
-app.get('/store',(req,res)=>{
-res.send('Store')
-})
-app.get('/store/products',(req,res)=>{
-res.send('Products')
-})
-// 
+// cors
 
-app.listen(port,()=>{
-    console.log(`hellow ${port}`);
-})
+app.use(cors());
+app.use(express.json());
+
+// array of object
+const arr = [];
+
+// get
+app.get("/api/contacts/Users", (req, res) => {
+  res.send(arr);
+});
+
+// post
+app.post("/api/contacts/Users", (req, res) => {
+  const { title } = req.body;
+  arr.push({
+    title: title,
+    id: Date.now(),
+  });
+});
+
+// delete
+app.delete("/api/contacts/Users/:id", (req, res) => {
+  const { id } = req.params;
+  const index = arr.findIndex((user) => user.id === Number(id));
+  if (index === -1) {
+    return;
+  }
+  arr.splice(index, 1);
+});
+
+// update todo
+app.put("/api/contacts/Users/:id", (req, res) => {
+  const { title } = req.body;
+  const { id } = req.params;
+  const index = arr.findIndex((user) => user.id === Number(id));
+  if (index === -1) {
+    return;
+  }
+  arr[index].title = title;
+});
+
+app.listen(port, () => {
+  console.log(`hellow ${port}`);
+});
